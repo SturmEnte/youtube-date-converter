10895b89a3beb4b2f53f21a2fbc7ed6d1e744fd4
SturmEnte <sturmente@proton.me>
2022-07-16 02:12:27 +0200

    First working version

--- plugin.js
+++ plugin.js
@@ -1,1 +1,29 @@
 console.log("Youtube Date Converter");
+
+const month = {
+	jan: 1,
+	feb: 2,
+	mar: 3,
+	apr: 4,
+	may: 5,
+	jun: 6,
+	jul: 7,
+	aug: 8,
+	sep: 9,
+	oct: 10,
+	nov: 11,
+	dec: 12,
+};
+
+setInterval(() => {
+	let newDateString = "";
+	const dateObject = document.getElementById("info-strings").children[1];
+
+	newDateString += dateObject.innerHTML.split(" ")[1].replace(",", "");           // Day
+	newDateString += ".";
+	newDateString += month[dateObject.innerHTML.split(" ")[0].toLocaleLowerCase()]; // Month (Number)
+	newDateString += ".";
+	newDateString += dateObject.innerHTML.split(" ")[2];                            // Year
+
+	dateObject.innerHTML = newDateString;
+}, 500);
